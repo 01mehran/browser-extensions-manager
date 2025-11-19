7a9f2d9e684ee68a5cb09358d9e554cde161b477
mehran <mehranmohamadi1311@gmail.com>
2025-11-20 00:20:46 +0330

    style(header): add dark mode style for header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,15 @@ import moon from '@images/icon-moon.svg';
 
 export default function Header() {
   return (
-    <header className="bg-primary-light-100 flex items-center justify-between rounded-xl p-2 shadow-sm">
+    <header className="bg-primary-light-100 dark:bg-primary-dark-800 flex items-center justify-between rounded-xl p-2 shadow-sm">
       <article className="w-36">
         <img
           src={logo}
           alt="project logo"
-          className="h-full w-full object-cover"
+          className="h-full w-full object-cover dark:brightness-100 dark:invert"
         />
       </article>
-      <article className="bg-primary-light-200/80 hover:bg-primary-light-200 cursor-pointer rounded-lg p-2 transition-all duration-200 focus-within:outline-red-600">
+      <article className="bg-primary-light-200/80 hover:bg-primary-light-200 dark:hover:bg-primary-dark-600 dark:bg-primary-dark-700 cursor-pointer rounded-lg p-2 transition-all duration-200 focus-within:outline-red-600">
         <img
           src={moon}
           alt="moon/sun icon"
